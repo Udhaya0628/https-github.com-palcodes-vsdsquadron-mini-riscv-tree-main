6b59273c95cbaec3b05f57ffbe4b56fe4c484b80
Udhaya0628 <vijisekar7072@gmail.com>
2026-05-18 17:38:37 +0530

    Updated Task 2 UART GPIO with successful upload

--- task2_uart_gpio/src/main.c
+++ task2_uart_gpio/src/main.c
@@ -1,8 +1,8 @@
 #include <ch32v00x.h>
 #include <debug.h>
 
-#define BLINKY_GPIO_PORT GPIOD
-#define BLINKY_GPIO_PIN GPIO_Pin_6
+#define LED_PORT GPIOD
+#define LED_PIN  GPIO_Pin_6
 
 int main(void)
 {
@@ -13,18 +13,18 @@ int main(void)
 
     GPIO_InitTypeDef GPIO_InitStructure = {0};
 
-    GPIO_InitStructure.GPIO_Pin = BLINKY_GPIO_PIN;
+    GPIO_InitStructure.GPIO_Pin = LED_PIN;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
-    GPIO_Init(BLINKY_GPIO_PORT, &GPIO_InitStructure);
+    GPIO_Init(LED_PORT, &GPIO_InitStructure);
 
     while (1)
     {
-        GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, Bit_SET);
+        GPIO_WriteBit(LED_PORT, LED_PIN, Bit_SET);
         Delay_Ms(500);
 
-        GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, Bit_RESET);
+        GPIO_WriteBit(LED_PORT, LED_PIN, Bit_RESET);
         Delay_Ms(500);
     }
 }
